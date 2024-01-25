fb747f0d64eceba7e3314f81606f918d94fd04b8
SopaSupu <142053856+SopaSupu@users.noreply.github.com>
2024-01-25 02:11:03 -0600

    jardin

--- proyecto 3D progra2/Assets/scripts/CaminadoDireccional.cs
+++ proyecto 3D progra2/Assets/scripts/CaminadoDireccional.cs
@@ -4,52 +4,39 @@ using UnityEngine;
 
 public class CaminadoDireccional : MonoBehaviour
 {
-    // crear atributos para el rigidbody, 
-
-    public float Velocidad = 5;
     private Rigidbody miCuerpo;
     private Animator miAnimador;
+    public int veloPerso = 5;
+    public int rotSuav = 1;
 
+    // Start is called before the first frame update
     void Start()
     {
         miCuerpo = GetComponent<Rigidbody>();
         miAnimador = GetComponent<Animator>();
     }
 
-
+    // Update is called once per frame
     void Update()
     {
-        //leer valor d einput, de los ejes horizontal y vertical y guardarlos en vars
+        float movHoriz = Input.GetAxisRaw("Horizontal");
+        float movVert = Input.GetAxisRaw("Vertical");
 
-        //Crear un nuevo vector llamado direccion y le asigno el valor horizontal a "x" y el valor vertical a "z"
-
-
-        //Asignar el vector de direccion a la proipiedad forward del transform
-
-        //Asignar al rb la velocidad a partir de multiplicar el vector forward por la magnitud del vector
-        //de direccion por el parametro de velocidad
-
-        //si la magnitud del vector de direccion es igual a 0, activo en animator el parametro boleano de caminado, si no, lo desactivo
-
-        float movHoriz = Input.GetAxis("Horizontal");
-        float movVert = Input.GetAxis("Vertical");
-
-        Vector3 dir = new Vector3(movHoriz, 0, movVert);
-
-       
-       
+        Vector3 direccion = new Vector3(movHoriz, 0, movVert);
 
 
-        if(dir.magnitude > 0)
+
+        if (direccion.magnitude > 0)
         {
+            Vector3 nuevDir = Vector3.Lerp(transform.forward, direccion, Time.deltaTime * rotSuav);
+            transform.forward = nuevDir;
             miAnimador.SetBool("CAMINANDO", true);
-            transform.forward = dir;
         }
         else
         {
             miAnimador.SetBool("CAMINANDO", false);
         }
 
-        miCuerpo.velocity = (transform.forward * Velocidad) * (dir.magnitude);
+        miCuerpo.velocity = ((transform.forward * veloPerso) * direccion.magnitude);
     }
 }
